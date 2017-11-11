7d830d145ea47d6e317fc9ba1b940f22f58fdbea
Spikeone <fvv1@gmx.de>
2017-11-11 23:18:44 +0100

    build now works without moving files

--- Compiler/Compile_PowerHeroes.bat
+++ Compiler/Compile_PowerHeroes.bat
@@ -1,2 +1,7 @@
+copy "..\PowerHeroes.ps1" "PowerHeroes.ps1"
 call "callPS2EXE.bat" "PowerHeroes.ps1" "PowerHeroes.exe"
+copy "PowerHeroes.exe" "..\PowerHeroes.exe"
+del "PowerHeroes.exe"
+del "PowerHeroes.exe.config"
+del "PowerHeroes.ps1"
 pause
